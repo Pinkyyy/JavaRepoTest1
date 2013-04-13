a406334804f62be0ed712791baac02dcb54728d7
Dorota <Dorota@pinkyyy>
2013-04-13 13:42:25 +0200

    test 1

--- nbp/src/com/eurojava/nbp.java
+++ nbp/src/com/eurojava/nbp.java
@@ -2,18 +2,30 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com;
+package com.eurojava;
+
+import java.io.FileReader;
 
 /**
  *
  * @author Dorota
  */
-public class eurojava {
+public class nbp {
 
+    public nbp() {
+    }
+    
+    public void LoadFile(String fileName){
+        FileReader filereader = new FileReader(fileName);
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+    
 }
